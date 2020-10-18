ba3d42d5d2535d36351f00f0d8b4d7464c3f6d52
Ioannis Karamanolakis <ik2414@columbia.edu>
2020-10-18 15:17:05 -0400

    add scripts for running all CLS experiments

---
+++ scripts/run_cls.sh
@@ -0,0 +1,3 @@
+python ../clts/main.py --dataset cls --target_language french --num_seeds 10
+python ../clts/main.py --dataset cls --target_language german --num_seeds 10
+python ../clts/main.py --dataset cls --target_language japanese --num_seeds 10
